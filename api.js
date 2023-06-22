cb930590957995698212419238b2bc58df7cffa8
madeosha <lojkina95@yandex.ru>
2023-06-22 21:39:27 +0500

    Реализует форму регистрации

--- api.js
+++ api.js
@@ -1,46 +1,59 @@
 const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
 
-export function getTodos ({ token }) {
-    return fetch(host, {
-        method: "GET",
-        headers: {
-          Authorization: token,
-        },
-    }).then((response) => {
-        if (response.status === 401) {
-    // token = prompt("Введите верный пароль");
-    // fetchTodosAndRender();
-        throw new Error("Нет авторизации");
-        }
+export function getTodos({ token }) {
+  return fetch(host, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    if (response.status === 401) {
+      throw new Error("Нет авторизации");
+    }
+
     return response.json();
-    })
+  });
 }
 
-export function deleteTodo ({ token, id }) {
-    return fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
-      method: "DELETE",
-      headers: {
-        Authorization: token,
-      },
-    })
-      .then((response) => {
-        return response.json();
-      })
+export function deleteTodo({ token, id }) {
+  return fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    return response.json();
+  });
 }
 
-export function addTodo ({ text, token }) {
-    return fetch(host, {
-        method: "POST",
-        body: JSON.stringify({
-          text,
-        }),
-        headers: {
-          Authorization: token,
-        },
-      })
-        .then((response) => {
-          return response.json();
-        })
+export function addTodo({ text, token }) {
+  return fetch(host, {
+    method: "POST",
+    body: JSON.stringify({
+      text,
+    }),
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+export function registerUser({ login, password, name }) {
+  return fetch("https://webdev-hw-api.vercel.app/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+      name,
+    }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Такой пользователь уже существует");
+    }
+    return response.json();
+  });
 }
 
 export function loginUser({ login, password }) {
